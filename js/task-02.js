27491afaa995a41692014bebb09228f969a9ba4f
Dmytro Yermolenko <dima.ermol22@gmail.com>
2023-03-28 22:59:35 +0300

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulEl = document.querySelector('ul#ingredients');
+
+const createListEL = ingredient => {
+  const listEl = document.createElement('li');
+  listEl.textContent = `${ingredient}`;
+  listEl.classList.add('item');
+  
+  return listEl;
+}
+
+const listELementsEl = ingredients.map(createListEL);
+
+ulEl.append(...listELementsEl);
