31e89dc89a4f22e77df895406d60b1f4bf5e7ec6
dmitriy-student <dima.ermol22@gmail.com>
2023-04-01 22:04:30 +0300

    task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const formEL = document.querySelector('.login-form');
 
+formEL.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit (event) {
+    const formData = new FormData(event.currentTarget);
+    event.preventDefault();
+
+    if (!formData.get('email') || !formData.get('password')) {
+        alert('All fields must be filled in!');
+        return;
+      }
+    
+
+    formData.forEach((value, name) => {
+        console.log(name);
+        console.log(value);
+
+    })
+
+    formEL.reset();
+
+}
