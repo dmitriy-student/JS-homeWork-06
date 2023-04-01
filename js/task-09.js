28a0a24cdd7c6d18748c571f4ebf07e234006f11
dmitriy-student <dima.ermol22@gmail.com>
2023-04-01 22:10:17 +0300

    task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,19 @@
+const refs = {
+  colorSpan: document.querySelector('.color'),
+  changeColorButton: document.querySelector('.change-color'),
+  body: document.body,
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+refs.changeColorButton.addEventListener('click', onChangeColor);
+
+function onChangeColor () {
+  const newColor = getRandomHexColor();
+  refs.body.style.backgroundColor = newColor;
+  refs.colorSpan.textContent = newColor;
 }
