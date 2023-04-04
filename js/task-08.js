7fa211a0671647436bcfa5668c2c3d17cfa63cbc
dmitriy-student <dima.ermol22@gmail.com>
2023-04-04 20:57:08 +0300

    task 8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,23 +1,48 @@
+// const formEL = document.querySelector('.login-form');
+
+// formEL.addEventListener('submit', onFormSubmit);
+
+// function onFormSubmit (event) {
+//     const formData = new FormData(event.currentTarget);
+//     event.preventDefault();
+
+//     if (!formData.get('email') || !formData.get('password')) {
+//         alert('All fields must be filled in!');
+//         return;
+//       }
+    
+
+//     formData.forEach((value, name) => {
+//         console.log(name);
+//         console.log(value);
+
+//     })
+
+//     formEL.reset();
+
+// }
+
 const formEL = document.querySelector('.login-form');
 
 formEL.addEventListener('submit', onFormSubmit);
 
-function onFormSubmit (event) {
-    const formData = new FormData(event.currentTarget);
-    event.preventDefault();
+function onFormSubmit(event) {
+  event.preventDefault();
 
-    if (!formData.get('email') || !formData.get('password')) {
-        alert('All fields must be filled in!');
-        return;
-      }
-    
+  const email = formEL.elements.email.value;
+  const password = formEL.elements.password.value;
 
-    formData.forEach((value, name) => {
-        console.log(name);
-        console.log(value);
+  if (!email || !password) {
+    alert('All fields must be filled in!');
+    return;
+  }
 
-    })
+  const formData = {
+    email,
+    password,
+  };
 
-    formEL.reset();
+  console.log(formData);
 
+  formEL.reset();
 }
